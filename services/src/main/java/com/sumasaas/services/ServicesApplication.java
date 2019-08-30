eb3f51a2ef7a9df188cbe348e7f7688aa1ffdc09
juan correa <juancorrea@juans-MacBook-Pro.local>
2019-08-30 16:59:15 -0500

    url params listener

--- services/src/main/java/com/sumasaas/services/ServicesApplication.java
+++ services/src/main/java/com/sumasaas/services/ServicesApplication.java
@@ -4,8 +4,10 @@ import com.sumasaas.models.ConfigurationSubscriber;
 import com.sumasaas.repository.ConfigurationSubscriberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sumasaas")
 public class ServicesApplication {
 
     public static void main(String[] args) {
--- services/src/main/java/com/sumasaas/services/sqs/SqsListeners.java
+++ services/src/main/java/com/sumasaas/services/sqs/SqsListeners.java
@@ -1,5 +1,6 @@
 package com.sumasaas.services.sqs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SqsListeners {
 
-    @SqsListener(value = "https://sqs.us-east-2.amazonaws.com/087627071411/Cotizaciones_completas", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @SqsListener(value = "${cloud.aws.end-point.uri}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void getMessage(Object message) {
         System.out.println("Message from SQS Queue - "+message);
     }
